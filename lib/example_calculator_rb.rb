9679638b6bc3c40439c659ad578cca24d64f46b9
Daniel Huffman <drhuffman12@gmail.com>
2019-04-15 23:25:25 -0400

    create new gem library (`bundle gem example_calculator_rb`)

---
+++ lib/example_calculator_rb.rb
@@ -0,0 +1,6 @@
+require "example_calculator_rb/version"
+
+module ExampleCalculatorRb
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/example_calculator_rb/version.rb
@@ -0,0 +1,3 @@
+module ExampleCalculatorRb
+  VERSION = "0.1.0"
+end
