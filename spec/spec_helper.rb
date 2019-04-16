0f8a7f95c4c99a6c3132d55baf5ffb91f4d3ddf4
Daniel Huffman <drhuffman12@gmail.com>
2019-04-16 19:01:24 -0400

    make rubocop happy

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,11 @@
-require "bundler/setup"
+require 'bundler/setup'
 require 'simplecov'
 
-require "example_calculator"
+require 'example_calculator'
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
